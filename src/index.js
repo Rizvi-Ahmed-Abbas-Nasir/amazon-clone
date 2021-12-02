7fc0da718323b671656efc7519df28d7a94daf6a
Rizvi-Ahmed-Abbas_Nasir <abbasrizvi200209@gmail.com>
2021-12-03 03:32:55 +0530

    New

--- src/index.js
+++ src/index.js
@@ -11,6 +11,13 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+
+var number = [2,4,5,6,7,8];
+
+const newArray = number.map((x) => x*x); //ES6 => is Fat Arrow Function
+
+console.log(newArray);
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
