5fbe60df83b9d6b0c1448a6c9e2080f434a466eb
Rizvi-Ahmed-Abbas_Nasir <abbasrizvi200209@gmail.com>
2021-12-12 04:22:33 +0530

    New

--- src/Subtotal.js
+++ src/Subtotal.js
@@ -1,16 +1,31 @@
 import React from 'react'
-import './Subtotal.css'
-
+import { useStateValue } from './StateProvider';
+import './Subtotal.css';
+import CurrencyFormat from 'react-currency-format';
 function Subtotal() {
+    const  [{cart}, dispatch] = useStateValue();
     return (
         <div className="Subtotal">
-            <p>Subtotal (0 Item ): <strong>0</strong></p>
+
+
+        <CurrencyFormat
+            renderText= {(value) => (
+            <>
+
+            <p>Subtotal ({cart.length} Item ): <strong></strong></p>
             <small className="Subtotal__gift">
                 <input type="checkbox" /> This Order Contain a Gift
             </small>
+       </>
+    )}
+    decimalScale = {2}
+    value = {0}
+    displayType = {"text"}
+    prefix= {"₹"}
+/>
             <button>Proced To Buy</button>
         </div>
-    )
+    );
 }
 
 export default Subtotal
