8cfd0cf482cd1d343c020a4ac4ea965443582599
Rizvi-Ahmed-Abbas_Nasir <abbasrizvi200209@gmail.com>
2022-01-17 15:13:21 +0530

    new

--- src/Login.js
+++ src/Login.js
@@ -6,8 +6,24 @@ function Login() {
     return (
         <div className='login'>
             <Link to = "/">
-            <img className="Login__IMAGE" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSRlYzExQDYgc7iygaVwmCvdy1YKKjUBvI2cGKZmDFzVxXoOSD2jrtZMBEsVM0C3snD9n4&usqp=CAU" alt="amazon" />  
+            <img className="Login__IMAGE" src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/2560px-Amazon_logo.svg.png" alt="amazon" />  
             </Link>
+
+            <div className='login--container'>
+                <h1 >Sign In</h1>
+                <form>
+                    <h5>E-mail</h5>
+                    <input type='Text' />
+                    <h5>Password </h5>
+                    <input  type='password'/>
+                    <button className='sign-in-button'>Sign in</button>
+                </form>
+                <p>
+                    By Continuig, you agree to Amazon's Conditon of use and Privacy Notice, Our Cookies Notice and our Interest Based Ads Notices
+                </p>
+                <button className='regsisterbutton'>Create you Amazon Account</button>
+
+            </div>
         </div>
     )
 }
