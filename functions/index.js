5227395c88b2e369ba8c7034bf0a407ec402b83a
Rizvi-Ahmed-Abbas_Nasir <abbasrizvi200209@gmail.com>
2022-02-15 12:34:31 +0530

    New

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,41 @@
 const functions = require("firebase-functions");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const express = require("express"); // npm i express (install)
+
+const cors = require("cors"); // npm i cors (install)
+const { response, request } = require("express");
+
+const stripe = require("stripe")("sk_test_51KMbTZSBGjHkWRO32kVQ85Tfxs9iH3iZLDlUAyf2ir01Nmr2G6SKTKLSogykv2hcYEshHkCoY4N8RY0BAE2qYliZ00Qaqw4P4R"); //npm i stripe (Install) 
+
+// API
+
+// App Config setting up express server
+const app = express();
+
+// Middlewares
+app.use(cors({origin: true}));
+app.use(express.json());
+
+// API Routes
+app.get('/',(request, response)=>response.status(200).send("hello world"))
+
+app.get('/Abbas',(request, response)=>response.status(200).send("Rizvi Ahmed"))
+
+app.post('/payments/create', async(request, response) =>{
+    const total = request.query.total;
+    console.log("Total Payment Received>>",total);
+
+    const paymentIntent = await stripe.PaymentIntent.create({
+        amount: total, // paise Value not in Rupess
+        currency: "inr",
+    })
+})
+
+// Listen Cammond
+
+exports.api = functions.https.onRequest(app);
+
+// => In Terminal Type (firebase emulators: start) to start emulators
+
+// EndPoint
+// http://localhost:5001/clone-f4f6a/us-central1/api
